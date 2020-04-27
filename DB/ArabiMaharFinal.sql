072469ecdc33f3ae1f444f91a50dd0b3fb16e7fe
amin <amin_kasiri@yahoo.com>
2020-04-27 16:04:59 +0430

    DB recovered

--- DB/ArabiMaharFinal.sql
+++ DB/ArabiMaharFinal.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `arabimahar` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `arabimahar`;
 -- MySQL dump 10.13  Distrib 8.0.14, for Win64 (x86_64)
 --
 -- Host: localhost    Database: arabimahar
@@ -216,4 +218,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-27 12:42:19
+-- Dump completed on 2020-04-27 16:02:55
